04562adb177031c58a48b6193db521d1eb57d1c1
kone-yaya-dev <64926396+kone-yaya-dev@users.noreply.github.com>
2023-01-26 13:46:20 +0000

    first commit

---
+++ DI_BOOTCAMP_Week5_Day1_XP_NINJA-main/Exercise_2_Ionic_HTTP_Request/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Exercise_2_Ionic_HTTP_Request',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
